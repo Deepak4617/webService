f267205f35734905f6480e015b7288ec3a355e31
Deepak4617 <dk4803382@gmail.com>
2024-06-05 15:40:51 +0530

    add

--- index.js
+++ index.js
@@ -6,32 +6,12 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const User = require('./user')
 
-// User model schema
-// const userSchema = new mongoose.Schema({
-//   firstName: String,
-//   lastName: String,
-//   mobileNo: String,
-//   email: String,
-//   address: {
-//     street: String,
-//     city: String,
-//     state: String,
-//     country: String,
-//   },
-//   loginId: String,
-//   password: String,
-// });
-
-// const User = mongoose.model('User', userSchema);
-
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// Replace with your MongoDB connection string
 mongoose.connect('mongodb+srv://dk4803382:ironman123@createuser.ockznqh.mongodb.net/?retryWrites=true&w=majority&appName=Createuser', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -39,7 +19,6 @@ mongoose.connect('mongodb+srv://dk4803382:ironman123@createuser.ockznqh.mongodb.
   .then(() => console.log('Connected to MongoDB'))
   .catch(error => console.error('Connection error:', error));
 
-// Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
 // API endpoint to create a new user
@@ -83,46 +62,23 @@ app.get('/api/users', async (req, res) => {
   }
 });
 
-// Endpoint to delete a user
-app.delete('/api/users/:email', async (req, res) => {
-  try {
-    const user = await User.findOneAndDelete({ email: req.params.email });
-    if (user) {
-      io.to('live_users').emit('userRemoved', { email: user.email, socketId: user._id.toString() });
-      res.status(200).json({ message: 'User deleted successfully' });
-    } else {
-      res.status(404).json({ message: 'User not found' });
-    }
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-
-
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+const io = require('socket.io')(server);
+
+let users = [];
 
 io.on('connection', (socket) => {
-  socket.on('join_live_users', async (data) => {
-    const { email } = data;
-    socket.join('live_users');
-
-    try {
-      const users = await User.find({}, 'email _id');
-      const userData = users.map(user => ({ email: user.email, socketId: socket.id }));
-      socket.emit('current_users', userData);
-    } catch (error) {
-      console.error('Error fetching users:', error);
+  socket.on('join_live_users', ({ email }) => {
+    if (!users.find(user => user.email === email)) {
+      const user = { email, socketId: socket.id };
+      users.push(user);
+      io.emit('userJoined', { email, socketId: socket.id, allUsers: users });
+    } else {
+      socket.emit('userJoined', { email, socketId: socket.id, allUsers: users });
     }
-    
-    io.to('live_users').emit('userJoined', { email, socketId: socket.id });
-  });
-
-  socket.on('disconnect', () => {
-    io.to('live_users').emit('userDisconnected', socket.id);
   });
 });
 
